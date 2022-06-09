5079997f32eb4fa3311a00c84769c2d6241e2c33
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-06-09 19:25:07 +0400

    :test_tube: Tests for secure response

--- api/tests/gettoken.test.ts
+++ api/tests/gettoken.test.ts
@@ -20,11 +20,61 @@ describe('GetToken Endpoint', () => {
     )) as FormattedReturn;
 
     expect(response.statusCode).toBe(200);
-    const { access_token, refresh_token } = JSON.parse(response.body);
+    const { access_token } = JSON.parse(response.body);
     expect(access_token).toBeDefined();
     expect(access_token).toBe(100);
     expect(response.headers['Set-Cookie']).toContain('refresh_token=200');
   });
-});
+  it('should not return the refresh token in body', async () => {
+    /* @ts-ignore */
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ access_token: 100, refresh_token: 200 }),
+        ok: true,
+      } as Response)
+    );
+    const response = (await handler(
+      { body: JSON.stringify({ code: 'valid' }) } as any,
+      {} as any
+    )) as FormattedReturn;
 
-/* {"access_token":"BQDHkMk7glOhFSDd35_bMXH42-kd1yOViq2N6FikNeIZ_M8rDUJOgjtyTZdAgL--5N33r2Gc2kOo4j53fy7NEM1QkxuLybdmeOjEE-5AW692el_4Ay9i4RvbHCCHrV7hP8c0lJiqAFVmZvYMS_nOMj9GtwpZWwdPCRg3OAWPu3r7ufHUD1cf3nwyBMh78LIdxtOiFYREEJqYIGVl8KMD-3GVrFZoJiH7rFt9znCq","token_type":"Bearer","expires_in":3600,"refresh_token":"AQB8RwrxTmtdHkIiVNF9A0rKnUsyfwMZ4iK062cp-etfOftFTXsWbQ4yR0efHSxPnJnHUwGnefC8RSEV9Sed01Leavp-KGPoXLaqe8hm5pnAO3UV53sabOGy1dkTDJvTYjA","scope":"streaming user-modify-playback-state user-follow-modify user-library-read user-library-modify user-follow-read user-read-playback-state user-read-currently-playing user-read-recently-played user-read-email user-top-read user-read-private"} */
+    expect(response.statusCode).toBe(200);
+    const { refresh_token } = JSON.parse(response.body);
+    expect(refresh_token).toBeUndefined();
+  })
+
+  it('should return a secure HTTP cookie containing the refresh token', async () => {
+    /* @ts-ignore */
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({ access_token: 100, refresh_token: 200 }),
+        ok: true,
+      } as Response)
+    );
+    const response = (await handler(
+      { body: JSON.stringify({ code: 'valid' }) } as any,
+      {} as any
+    )) as FormattedReturn;
+
+    expect(response.statusCode).toBe(200);
+    expect(response.headers['Set-Cookie']).toContain('refresh_token=200');
+    expect(response.headers['Set-Cookie']).toContain('HttpOnly');
+    expect(response.headers['Set-Cookie']).toContain('Secure');
+  })
+  it('should return 418 when code is bad', async () => {
+    /* @ts-ignore */
+    fetch.mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve({}),
+        ok: false,
+      } as Response)
+    );
+    const response = (await handler(
+      { body: JSON.stringify({ code: 'invalid' }) } as any,
+      {} as any
+    )) as FormattedReturn;
+
+    expect(response.statusCode).toBe(418)
+    expect(JSON.parse(response.body).error).toBe('Error fetching token');
+   })
+});
