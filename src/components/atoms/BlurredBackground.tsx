85041d0e59e8e3dfd04462391be26b7fa3c70d1c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-06-15 18:15:20 +0400

    :construction: Hides Fancy Background

--- src/components/atoms/BlurredBackground.tsx
+++ src/components/atoms/BlurredBackground.tsx
@@ -8,14 +8,14 @@ export const BlurredBackground = ({
 }): JSXInternal.Element => {
   return (
     <div class="fixed inset-0">
-      {src && false && (
+      {/* {src && (
         <img
           src={src}
           class="absolute -inset-4 min-h-[calc(100vh+32px)] min-w-[calc(100vw+32px)] object-cover opacity-60 blur-lg"
           loading="lazy"
         />
       )}
-      <AnimatedWaves layers={3} />
+      <AnimatedWaves layers={3} /> */}
       <div class="absolute inset-0 bg-gradient-to-tr from-neutral-900 to-transparent opacity-75" />
       <div class="absolute inset-0 bg-neutral-900 opacity-50" />
     </div>
