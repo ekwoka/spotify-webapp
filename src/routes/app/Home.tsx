a40c009f07ef1d850b3882bdfdd99e7c84ae0740
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-06-15 18:15:30 +0400

    :construction: Limits API Calls

--- src/routes/app/Home.tsx
+++ src/routes/app/Home.tsx
@@ -9,7 +9,7 @@ export const Home = (): JSXInternal.Element => {
     <div class="flex flex-col gap-8">
       <div className="flex flex-col gap-4">
         <h2>Most Listened Tracks</h2>
-        <TrackList type="getMyTopTracks" limit={50}>
+        <TrackList type="getMyTopTracks" limit={10}>
           <SimpleFlexGrid
             as={(item) => <SimpleGridItem {...item} />}
             items={[] as TrackObject[]}
@@ -19,7 +19,7 @@ export const Home = (): JSXInternal.Element => {
       </div>
       <div className="flex flex-col gap-4">
         <h2>Recently Played tracks</h2>
-        <TrackList type="getMyRecentlyPlayedTracks" limit={50}>
+        <TrackList type="getMyRecentlyPlayedTracks" limit={10}>
           <SimpleFlexGrid
             as={(item) => <SimpleGridItem {...item} />}
             items={[] as TrackObject[]}
