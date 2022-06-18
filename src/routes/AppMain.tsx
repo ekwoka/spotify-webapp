3b58c0ea854edb38fe8b2e67e81a44ef83c2d874
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-06-18 16:38:17 +0400

    :arrow_up: Dependencies

--- src/routes/AppMain.tsx
+++ src/routes/AppMain.tsx
@@ -1,5 +1,4 @@
-import { useMemo, useState } from 'preact/hooks';
-import { usePlayer } from '../hooks';
+import { useState } from 'preact/hooks';
 import { BlurredBackground } from '../components/atoms/BlurredBackground';
 import { MainSection } from './app/MainSection';
 import {
@@ -9,20 +8,10 @@ import {
 
 export const Main = () => {
   const [sideBarIsOpen, setSideBarIsOpen] = useState<boolean>(false);
-  const playerState = usePlayer()[2];
-
-  const bgImage = useMemo(() => {
-    if (!playerState?.track_window?.current_track?.album?.images[0])
-      return null;
-    const images = playerState.track_window.current_track.album.images;
-    return images.reduce((largest, image) =>
-      image.width > largest.width ? image : largest
-    );
-  }, [playerState]);
 
   return (
     <div>
-      <BlurredBackground src={bgImage?.url} />
+      <BlurredBackground />
       <MobileSideBar isOpen={sideBarIsOpen} setIsOpen={setSideBarIsOpen} />
       <DeskTopSideBar />
       <MainSection />
