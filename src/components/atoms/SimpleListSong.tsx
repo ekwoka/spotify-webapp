842fbddeeefa639f00fbd6b775cf8e7a9ffb37ef
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-06-30 13:21:46 +0400

    :construction: Improves Playlist Queue

--- src/components/atoms/SimpleListSong.tsx
+++ src/components/atoms/SimpleListSong.tsx
@@ -3,4 +3,22 @@ import { SpotifyWebPlaybackTrack } from 'spotify-web-playback';
 
 export const SimpleListSong = (
   props: SpotifyWebPlaybackTrack
-): JSXInternal.Element => <h3>{props.name}</h3>;
+): JSXInternal.Element => {
+  console.log(props);
+  return (
+    <div class="flex items-center gap-4 rounded-lg bg-transparent p-4 transition-colors hover:bg-neutral-800 hover:shadow">
+      <img
+        src={props.album.images[0]?.url ?? 'https://placekitten.com/300/300'}
+        class="h-20 w-20 flex-none rounded object-cover shadow"
+        loading="lazy"
+        width="1"
+        height="1"
+      />
+      <div class="flex grow basis-0 flex-col justify-center">
+        <h3 class="text-lg text-lime-500">{props.name}</h3>
+        <p class="text-neutral-300">{props.artists[0]?.name ?? ''}</p>
+      </div>
+      <p class="grow basis-0 text-neutral-300">{props.album?.name ?? ''}</p>
+    </div>
+  );
+};
