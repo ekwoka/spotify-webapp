840d32e829ccfd5dcbaeebfa930963a8972ee70f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-06-30 14:26:03 +0400

    :construction: Improves Queue List

--- src/components/atoms/SimpleListSong.tsx
+++ src/components/atoms/SimpleListSong.tsx
@@ -1,24 +1,37 @@
 import { JSXInternal } from 'preact/src/jsx';
-import { SpotifyWebPlaybackTrack } from 'spotify-web-playback';
 
-export const SimpleListSong = (
-  props: SpotifyWebPlaybackTrack
-): JSXInternal.Element => {
-  console.log(props);
+export const SimpleListSong = ({
+  name,
+  image,
+  album,
+  artist,
+  click,
+}: SimpleListSongProps): JSXInternal.Element => {
   return (
-    <div class="flex items-center gap-4 rounded-lg bg-transparent p-4 transition-colors hover:bg-neutral-800 hover:shadow">
+    <button
+      type="button"
+      class="flex items-center gap-4 rounded-lg bg-transparent p-4 transition-colors hover:bg-neutral-800 hover:shadow"
+      onClick={click}>
       <img
-        src={props.album.images[0]?.url ?? 'https://placekitten.com/300/300'}
+        src={image ?? 'https://placekitten.com/300/300'}
         class="h-20 w-20 flex-none rounded object-cover shadow"
         loading="lazy"
         width="1"
         height="1"
       />
       <div class="flex grow basis-0 flex-col justify-center">
-        <h3 class="text-lg text-lime-500">{props.name}</h3>
-        <p class="text-neutral-300">{props.artists[0]?.name ?? ''}</p>
+        <h3 class="text-lg text-lime-500">{name}</h3>
+        <p class="text-neutral-300">{artist}</p>
       </div>
-      <p class="grow basis-0 text-neutral-300">{props.album?.name ?? ''}</p>
-    </div>
+      <p class="grow basis-0 text-neutral-300">{album}</p>
+    </button>
   );
+};
+
+type SimpleListSongProps = {
+  name: string;
+  image?: string;
+  album: string;
+  artist: string;
+  click: () => void;
 };
--- src/routes/app/Playing.tsx
+++ src/routes/app/Playing.tsx
@@ -1,25 +1,68 @@
-import { useGlobalState } from '@ekwoka/preact-global-state';
 import { JSXInternal } from 'preact/src/jsx';
 import { SimpleListSong } from '../../components/atoms/SimpleListSong';
-import { PlayerState } from '../../hooks';
+import { useAsyncMemo, usePlayer, useSpotify } from '../../hooks';
+import { getBestImage, SpotifyImageArray } from '../../utils/getBestImage';
 
 export const Playing = (): JSXInternal.Element => {
-  const [state] = useGlobalState<PlayerState | null>('playerstate', null);
+  const [_, { play }, state] = usePlayer();
+  const spotifyApi = useSpotify();
+  const currentPlaylist = useAsyncMemo(
+    async () => {
+      if (!state?.context?.uri) return;
+      const playlistId = (state.context.uri as string).split(':playlist:')[1];
+      const playlist = await spotifyApi.getPlaylistTracks(playlistId);
+      return (playlist?.body?.items ?? []).map((item) => item.track);
+    },
+    [],
+    [state?.context?.uri]
+  );
+  const currentIndex = (currentPlaylist ?? []).findIndex(
+    (track) => track?.id === state?.track_window.current_track.id
+  );
+  const nextTracks =
+    currentIndex >= 0 ? currentPlaylist?.slice(currentIndex + 1) ?? [] : [];
+
+  const handlePlay = (offset: number) => {
+    if (!state?.context?.uri) return;
+    play(state.context.uri, offset);
+  };
+
   return (
     <section class="flex flex-col gap-8">
       <div class="flex flex-col gap-4">
         <h2 class="text-lg">Currently Playing</h2>
         <div class="flex flex-col gap-2">
           {state?.track_window.current_track && (
-            <SimpleListSong {...state.track_window.current_track} />
+            <SimpleListSong
+              key={state?.track_window.current_track?.id}
+              click={() => handlePlay(currentIndex)}
+              name={state?.track_window.current_track?.name ?? ''}
+              album={state?.track_window.current_track?.album?.name ?? ''}
+              artist={state?.track_window.current_track?.artists[0]?.name ?? ''}
+              image={
+                getBestImage(
+                  (state?.track_window.current_track?.album?.images ??
+                    []) as SpotifyImageArray
+                ) || 'https://placekitten.com/300/300'
+              }
+            />
           )}
         </div>
       </div>
       <div class="flex flex-col gap-4">
         <h2 class="text-lg">Up Next</h2>
         <div class="flex flex-col gap-2">
-          {state?.track_window.next_tracks.map((track) => (
-            <SimpleListSong key={track.id} {...track} />
+          {nextTracks.map((track, i) => (
+            <SimpleListSong
+              key={track?.id}
+              click={() => handlePlay(currentIndex + i + 1)}
+              name={track?.name ?? ''}
+              album={track?.album?.name ?? ''}
+              artist={track?.artists[0]?.name ?? ''}
+              image={getBestImage(
+                (track?.album?.images ?? []) as SpotifyImageArray
+              )}
+            />
           ))}
         </div>
       </div>
