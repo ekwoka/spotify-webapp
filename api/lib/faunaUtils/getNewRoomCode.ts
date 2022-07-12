4fa882fa7dc27497f72f15d31dbce4fd8f70447f
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-07-12 11:11:29 +0400

    :bug: Autocloses Fauna Connections

--- api/lib/faunaUtils/getNewRoomCode.ts
+++ api/lib/faunaUtils/getNewRoomCode.ts
@@ -1,11 +1,17 @@
 import { makeRoomCode } from '../../utils';
 import { fauna, query } from '../';
+import { Client } from 'faunadb';
 
 const { Match, Index, Exists } = query;
 
-export const getNewRoomCode = async (): Promise<string> => {
-  const code = makeRoomCode();
-  const exists = await fauna.query(Exists(Match(Index('roomWithCode'), code)));
-  if (exists) return getNewRoomCode();
-  return code;
-};
+export const getNewRoomCode = (): Promise<string> => {
+
+  const roomLoop = async (client: Client): Promise<string> => {
+    const code = makeRoomCode();
+    const exists = await client.query(Exists(Match(Index('roomWithCode'), code)));
+    if (exists) return roomLoop(client);
+    return code;
+  }
+
+  return fauna.openQuery<string>(roomLoop);
+}
