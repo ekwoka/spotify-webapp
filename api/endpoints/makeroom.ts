fb5673060934f94de31f9e10788e2e7908ca3981
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-07-12 11:21:15 +0400

    :construction: Makes rooms

--- api/endpoints/makeroom.ts
+++ api/endpoints/makeroom.ts
@@ -1,16 +1,17 @@
 import { formattedReturn, FormattedReturn } from '../utils/formattedReturn';
 import { Handler } from '@netlify/functions';
+import { getNewRoomCode } from '../lib/faunaUtils/getNewRoomCode';
 import { fauna, query } from '../lib';
 
 const { Create, Collection } = query;
-
 export const handler: Handler = async (): Promise<FormattedReturn> => {
   try {
-    const response = await fauna.query(
-      Create(Collection('party_rooms'), { data: { created_at: Date.now() } })
-    );
+    const code = await getNewRoomCode();
 
-    return formattedReturn(200, response);
+    const response = await fauna.query(
+      Create(Collection('party_rooms'), { data: { code, created_at: Date.now(), songs: [] } })
+    );
+    return formattedReturn(200, { code, response });
   } catch (e) {
     console.error(e);
     return formattedReturn(500, { error: e });
